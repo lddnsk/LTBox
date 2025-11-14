b6b8acb30132688de783818a2a072616a9e2f41c
Jeong Seongpil <16506343+jjhitel@users.noreply.github.com>
2025-11-14 21:12:15 +0900

    main: Remove global module state

--- ltbox/main.py
+++ ltbox/main.py
@@ -15,15 +15,6 @@ BASE_DIR = APP_DIR.parent
 PYTHON_EXE = BASE_DIR / "python3" / "python.exe"
 DOWNLOADER_PY = APP_DIR / "downloader.py"
 
-utils = None
-actions = None
-workflow = None
-device = None
-constants = None
-avb_patch = None
-
-COMMAND_MAP = {}
-
 def setup_console():
     system = platform.system()
     if system == "Windows":
@@ -57,7 +48,7 @@ def check_path_encoding():
             input(get_string("press_enter_to_exit"))
         sys.exit(1)
 
-def run_task(command, title, dev):
+def run_task(command, title, dev, command_map):
     os.system('cls' if os.name == 'nt' else 'clear')
     
     print("  " + "=" * 58)
@@ -73,7 +64,7 @@ def run_task(command, title, dev):
 
     try:
         with logging_context(log_file):
-            func_tuple = COMMAND_MAP.get(command)
+            func_tuple = command_map.get(command)
             if not func_tuple:
                 print(get_string("unknown_command").format(command=command), file=sys.stderr)
                 return
@@ -117,7 +108,7 @@ def run_task(command, title, dev):
         else:
             input()
 
-def run_info_scan(paths):
+def run_info_scan(paths, constants, avb_patch):
     print(get_string("scan_start"))
     
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -191,7 +182,7 @@ def print_advanced_menu():
     print(get_string("menu_adv_m"))
     print("\n  " + "=" * 58 + "\n")
 
-def advanced_menu(dev):
+def advanced_menu(dev, command_map):
     actions_map = {
         "1": ("convert", get_string("task_title_convert_rom")),
         "2": ("read_edl", get_string("task_title_dump_devinfo")),
@@ -212,7 +203,7 @@ def advanced_menu(dev):
 
         if choice in actions_map:
             cmd, title = actions_map[choice]
-            run_task(cmd, title, dev)
+            run_task(cmd, title, dev, command_map)
             if choice == "11":
                 sys.exit()
         elif choice == "m":
@@ -234,7 +225,7 @@ def print_root_menu():
     print("\n" + get_string("menu_root_m"))
     print("\n  " + "=" * 58 + "\n")
 
-def root_menu(dev):
+def root_menu(dev, command_map):
     actions_map = {
         "1": ("root_boot_only", get_string("task_title_root_file")),
         "2": ("root_device", get_string("task_title_root")),
@@ -246,7 +237,7 @@ def root_menu(dev):
 
         if choice in actions_map:
             cmd, title = actions_map[choice]
-            run_task(cmd, title, dev)
+            run_task(cmd, title, dev, command_map)
         elif choice == "m":
             return
         else:
@@ -256,9 +247,9 @@ def root_menu(dev):
             else:
                 input(get_string("press_enter_to_continue"))
 
-def main_loop():
+def main_loop(device_controller_class, command_map):
     skip_adb = False
-    dev = device.DeviceController(skip_adb=skip_adb)
+    dev = device_controller_class(skip_adb=skip_adb)
     
     actions_map = {
         "1": ("patch_all_wipe", get_string("task_title_install_wipe")),
@@ -273,14 +264,14 @@ def main_loop():
 
         if choice in actions_map:
             cmd, title = actions_map[choice]
-            run_task(cmd, title, dev)
+            run_task(cmd, title, dev, command_map)
         elif choice == "4":
-            root_menu(dev)
+            root_menu(dev, command_map)
         elif choice == "6":
             skip_adb = not skip_adb
             dev.skip_adb = skip_adb
         elif choice == "a":
-            advanced_menu(dev)
+            advanced_menu(dev, command_map)
         elif choice == "x":
             break
         else:
@@ -291,8 +282,6 @@ def main_loop():
                 input(get_string("press_enter_to_continue"))
 
 def entry_point():
-    global utils, actions, workflow, device, constants, COMMAND_MAP, avb_patch
-    
     setup_console()
     
     is_info_mode = len(sys.argv) > 1 and sys.argv[1].lower() == 'info'
@@ -325,27 +314,29 @@ def entry_point():
         from . import constants as c
         from .patch import avb as avb
         
-        utils, actions, workflow, device, constants, avb_patch = u, a, w, d, c, avb
+        COMMAND_MAP = {
+            "convert": (a.convert_images, {}),
+            "root_device": (a.root_device, {}),
+            "root_boot_only": (a.root_boot_only, {}),
+            "unroot_device": (a.unroot_device, {}),
+            "disable_ota": (a.disable_ota, {}),
+            "edit_dp": (a.edit_devinfo_persist, {}),
+            "read_edl": (a.read_edl, {}),
+            "write_edl": (a.write_edl, {}),
+            "read_anti_rollback": (a.read_anti_rollback, {}),
+            "patch_anti_rollback": (a.patch_anti_rollback, {}),
+            "write_anti_rollback": (a.write_anti_rollback, {}),
+            "clean": (u.clean_workspace, {}),
+            "modify_xml": (a.modify_xml, {"wipe": 0}),
+            "modify_xml_wipe": (a.modify_xml, {"wipe": 1}),
+            "flash_edl": (a.flash_edl, {}),
+            "patch_all": (w.patch_all, {"wipe": 0}),
+            "patch_all_wipe": (w.patch_all, {"wipe": 1}),
+        }
         
-        COMMAND_MAP.update({
-            "convert": (actions.convert_images, {}),
-            "root_device": (actions.root_device, {}),
-            "root_boot_only": (actions.root_boot_only, {}),
-            "unroot_device": (actions.unroot_device, {}),
-            "disable_ota": (actions.disable_ota, {}),
-            "edit_dp": (actions.edit_devinfo_persist, {}),
-            "read_edl": (actions.read_edl, {}),
-            "write_edl": (actions.write_edl, {}),
-            "read_anti_rollback": (actions.read_anti_rollback, {}),
-            "patch_anti_rollback": (actions.patch_anti_rollback, {}),
-            "write_anti_rollback": (actions.write_anti_rollback, {}),
-            "clean": (utils.clean_workspace, {}),
-            "modify_xml": (actions.modify_xml, {"wipe": 0}),
-            "modify_xml_wipe": (actions.modify_xml, {"wipe": 1}),
-            "flash_edl": (actions.flash_edl, {}),
-            "patch_all": (workflow.patch_all, {"wipe": 0}),
-            "patch_all_wipe": (workflow.patch_all, {"wipe": 1}),
-        })
+        device_controller_class = d.DeviceController
+        constants_module = c
+        avb_patch_module = avb
 
     except ImportError as e:
         print(f"[!] Error: Failed to import 'ltbox' package.", file=sys.stderr)
@@ -359,7 +350,7 @@ def entry_point():
     
     if is_info_mode:
         if len(sys.argv) > 2:
-            run_info_scan(sys.argv[2:])
+            run_info_scan(sys.argv[2:], constants_module, avb_patch_module)
         else:
             print(get_string("info_no_files_dragged"), file=sys.stderr)
             print(get_string("info_drag_files_prompt"), file=sys.stderr)
@@ -367,7 +358,7 @@ def entry_point():
         if platform.system() == "Windows":
             os.system("pause")
     else:
-        main_loop()
+        main_loop(device_controller_class, COMMAND_MAP)
 
 if __name__ == "__main__":
     entry_point()
